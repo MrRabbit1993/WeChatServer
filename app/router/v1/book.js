d87b0f383278405781d11fc536bcba930a55aaa4
mr rabbit <1993iweb@gmail.com>
2020-01-10 00:13:33 +0800

    断点调试

--- app/router/v1/book.js
+++ app/router/v1/book.js
@@ -1,6 +1,6 @@
 const Router = require("koa-router");
 const router = new Router();
-router.get("/a/b", (ctx, next) => {
+router.get("/v1/a/b", (ctx, next) => {
     ctx.body = { key: "hello koa book" }
 })
 module.exports = router;
--- app/router/v1/classic.js
+++ app/router/v1/classic.js
@@ -1,6 +1,10 @@
 const Router = require("koa-router");
 const router = new Router();
-router.get("/a/a", (ctx, next) => {
-    ctx.body = { key: "hello koa" }
+router.post("/v1/:id/classic/latest", (ctx, next) => {
+    const path = ctx.params;//获取url参数/v1/1/a/b
+    const query = ctx.request.query;//获取地址栏参数？param=1
+    const headers = ctx.request.header;//获取header
+    const body = ctx.request.body;//获取body
+    ctx.body = { key: "hello koa1" }
 })
 module.exports = router;
